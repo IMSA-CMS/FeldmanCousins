69b040c9af1f380e8d6e310bd94aaf7ff206b8e5
Michael Vayninger <mvayninger@imsa.edu>
2019-03-13 11:34:03 -0500

    Added Hyper_Plane class

---
+++ Frequentest-Analysis-CMS-master/CMS-Fequentest/Hyper_Plane.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "Hyper_Plane.h"
+#include "CMS-Frequentest-Analysis.h"
+
+Hyper_Plane::Hyper_Plane(std::vector<std::vector<float>> adataSet)
+{
+
+}
+
+Hyper_Plane::Hyper_Plane(std::vector<std::vector<float>> adataSet, std::vector<std::vector<float>> base_Points, Hyper_Plane base)
+{
+
+}
+
+bool Hyper_Plane::check_Point_Outside()
+{
+
+}
+
+std::vector<Hyper_Plane> Hyper_Plane::expand_Surface()
+{
+
+}
+
+Hyper_Plane::~Hyper_Plane()
+{
+}
---
+++ Frequentest-Analysis-CMS-master/CMS-Fequentest/Hyper_Plane.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+#include <random>
+
+class Hyper_Plane
+{
+private:
+	std::vector<std::vector<float>> dataSet;
+	std::vector<double> orthogonalVector;
+
+public:
+	Hyper_Plane(std::vector<std::vector<float>> adataSet);
+	Hyper_Plane(std::vector<std::vector<float>> adataSet, std::vector<std::vector<float>> base_Points, Hyper_Plane base);
+	//Checks if a point is outside of the current plane
+	bool check_Point_Outside(); 
+	//Constructs a vector of hyperplanes taking every combination of base points and a new point with largest orthogonal distance
+	std::vector<Hyper_Plane> expand_Surface();
+	~Hyper_Plane();
+};
+
